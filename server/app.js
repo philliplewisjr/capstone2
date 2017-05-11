b36b7c733c98240e0f22a835c5b0cedee2736a16
Phillip Lewis <phillip.lewisjr@gmail.com>
2017-05-11 15:58:02 -0500

    app js built

--- server/app.js
+++ server/app.js
@@ -19,3 +19,19 @@ app.use( (req, res, next) => {
   err.status = 404;
   next(err)
 })
+
+//user error
+app.use((err, req, res, next)=> {
+  res.status(err.status || 500);
+  res.json({
+    message: err.message,
+    error: {}
+  })
+})
+
+const port = process.env.PORT || 3000;
+app.list(port, ()=>{
+  console.log(process.evn.NODE_ENV)
+  console.log(`Listening on port ${port}`)
+});
+module.exports = app;
