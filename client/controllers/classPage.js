cdececdc8e589e80bd95d589db89b3e757759018
Phillip Lewis <phillip.lewisjr@gmail.com>
2017-05-21 22:20:44 -0500

    able to delete students for page

--- client/controllers/classPage.js
+++ client/controllers/classPage.js
@@ -1,12 +1,29 @@
-app.controller("ClassPageCtrl", function($scope, $location, studentFactory){
+app.controller("ClassPageCtrl", function($scope, $location, studentFactory, $http){
   console.log("class page controller")
 
   //get all students
   studentFactory.getData()
   .then((data)=>{
-    console.log("data", data)
+    // console.log("data", data)
+    let students = data.data;
+    console.log(students)
+    $scope.students = data.data
   })
-  .catch((err)=>{
-    console.log(err)
-  })
+
+  $scope.delete = function (id) {
+    $scope.id = id;
+    console.log(id)
+    $http.delete(`http://localhost:5000/api/v1/students/${id}`)
+    .then((data)=>{
+      console.log("student deleted", data.data)
+      $http.get(`http://localhost:5000/api/v1/students`)
+      .then((data)=> {
+        $scope.students = data.data
+      })
+      .catch((err)=>{
+        console.log(err)
+      })
+    })
+  }
+
 })
